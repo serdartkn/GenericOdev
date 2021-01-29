ba8ec6db751a3acfc06da4aeeacda2596ea90eb8
serdar tekin <kara_bulu@hotmail.com>
2021-01-29 20:15:38 +0300

    Proje dosyası ekle.

---
+++ MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericOdev
+{
+    class MyDictionary<T1, T2>
+    {
+        T1[] key;
+        T2[] value;
+        T1[] TempKey;
+        T2[] Tempvalue;
+
+        public MyDictionary()
+        {
+            key = new T1[0];
+            value = new T2[0];
+        }
+
+        public void Add(T1 keys, T2 values)
+        {
+            TempKey = key;
+            Tempvalue = value;
+
+            key = new T1[key.Length + 1];
+            value = new T2[value.Length + 1];
+
+            for (int i = 0; i < TempKey.Length; i++)
+            {
+                for (int j = 0; j < Tempvalue.Length; j++)
+                {
+                    key[i] = TempKey[i];
+                    value[j] = Tempvalue[j];
+                }
+            }
+
+            key[key.Length - 1] = keys;
+            value[value.Length - 1] = values;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> isimYas = new MyDictionary<string, int>();
+            isimYas.Add("Serdar", 24);
+            isimYas.Add("Sesdardar", 234);
+        }
+    }
+}
